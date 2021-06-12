17372799e98070c86ad86f94125252112bd7426c
TeeMak <makindet74@gmail.com>
2021-06-12 15:32:18 +0100

    Done with login and register endpoint

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->group(function(){
+    Route::post('login', [App\Http\Controllers\LoginController::class, 'login']);
+    Route::post('register', [App\Http\Controllers\RegisterController::class, 'register']);
     
-
 });
 
 
